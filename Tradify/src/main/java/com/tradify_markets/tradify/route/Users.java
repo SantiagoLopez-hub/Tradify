66dc11d0b18e8f2b3bc90e911887f6b71af650bd
Santiago Lopez Paredes <46728986+SantiagoLopez-hub@users.noreply.github.com>
2022-12-11 00:05:51 +0000

    Get user by ID

--- Tradify/src/main/java/com/tradify_markets/tradify/route/Users.java
+++ Tradify/src/main/java/com/tradify_markets/tradify/route/Users.java
@@ -1,12 +1,28 @@
 package com.tradify_markets.tradify.route;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tradify_markets.tradify.model.User;
+import com.tradify_markets.tradify.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/users")
 public class Users {
-    @GetMapping("/")
-    public String home() {
-        return "Hello World!";
+    private final UserRepository userRepository;
+
+    public Users(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("")
+    public List<User> users() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> user(@PathVariable Integer id) {
+        return userRepository.findById(id);
     }
 }
