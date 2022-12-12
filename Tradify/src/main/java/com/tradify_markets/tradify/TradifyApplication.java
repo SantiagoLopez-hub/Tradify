76a315cc73765c12cce481e739f4e3eb86976616
Santiago <silp4477@gmail.com>
2022-12-12 14:08:37 +0000

    Create testing data

--- Tradify/src/main/java/com/tradify_markets/tradify/TradifyApplication.java
+++ Tradify/src/main/java/com/tradify_markets/tradify/TradifyApplication.java
@@ -1,23 +1,32 @@
 package com.tradify_markets.tradify;
 
+import com.tradify_markets.tradify.model.Announcement;
 import com.tradify_markets.tradify.model.Role;
+import com.tradify_markets.tradify.model.Share;
 import com.tradify_markets.tradify.model.User;
+import com.tradify_markets.tradify.repository.AnnouncementRepository;
 import com.tradify_markets.tradify.repository.RoleRepository;
+import com.tradify_markets.tradify.repository.ShareRepository;
 import com.tradify_markets.tradify.repository.UserRepository;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class TradifyApplication {
     private final UserRepository userRepository;
-
     private final RoleRepository roleRepository;
+    private final ShareRepository shareRepository;
+    private final AnnouncementRepository announcementRepository;
 
-    public TradifyApplication(UserRepository userRepository, RoleRepository roleRepository) {
+    public TradifyApplication(UserRepository userRepository, RoleRepository roleRepository, ShareRepository shareRepository, AnnouncementRepository announcementRepository) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.shareRepository = shareRepository;
+        this.announcementRepository = announcementRepository;
     }
 
     public static void main(String[] args) {
@@ -112,6 +121,21 @@ public class TradifyApplication {
                             .city("Milan")
                             .postCode("E1 6B")
                             .country("UK")
+                            .build()
+            );
+
+            announcementRepository.save(
+                    Announcement.builder()
+                            .id(1)
+                            .title("Test")
+                            .content("Test")
+                            .build()
+            );
+            shareRepository.save(
+                    Share.builder()
+                            .id(1)
+                            .price(100)
+                            .announcements(List.of(announcementRepository.findById(1).get()))
                             .build()
             );
         };
--- Tradify/src/main/java/com/tradify_markets/tradify/route/Users.java
+++ Tradify/src/main/java/com/tradify_markets/tradify/route/Users.java
@@ -1,6 +1,8 @@
 package com.tradify_markets.tradify.route;
 
+import com.tradify_markets.tradify.model.Share;
 import com.tradify_markets.tradify.model.User;
+import com.tradify_markets.tradify.repository.ShareRepository;
 import com.tradify_markets.tradify.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +13,12 @@ import java.util.Optional;
 @RequestMapping("/users")
 public class Users {
     private final UserRepository userRepository;
+    private final ShareRepository shareRepository;
 
-    public Users(UserRepository userRepository) {
+    public Users(UserRepository userRepository,
+                 ShareRepository shareRepository) {
         this.userRepository = userRepository;
+        this.shareRepository = shareRepository;
     }
 
     @GetMapping("")
@@ -24,5 +29,10 @@ public class Users {
     @GetMapping("/{id}")
     public Optional<User> user(@PathVariable Integer id) {
         return userRepository.findById(id);
+    }
+
+    @GetMapping("/share")
+    public List<Share> share() {
+        return shareRepository.findAll();
     }
 }
