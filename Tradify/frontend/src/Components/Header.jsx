f9d59cddf76b6f279cbb4389a68fdec2fb5f650d
Santiago Lopez Paredes <46728986+SantiagoLopez-hub@users.noreply.github.com>
2022-12-23 23:14:09 +0000

    Add header with bootstrap

--- Tradify/frontend/src/Components/Header.jsx
+++ Tradify/frontend/src/Components/Header.jsx
@@ -1,6 +1,33 @@
+import {Link} from "react-router-dom";
+
 const Header = () => {
     return (
-        <p>Header</p>
+        <header className="sticky-top navbar
+            border-bottom navbar-expand-lg
+            bg-white navbar-light">
+            <h1>Tradify</h1>
+
+            <button className="navbar-toggler" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <ul id="navbarSupportedContent"
+                className="navbar-nav collapse me-auto navbar-collapse justify-content-end">
+                <li className="nav-item px-4">
+                    <Link className="nav-link" to="/">
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item px-4">
+                    <Link className="nav-link" to="/users">
+                        Users
+                    </Link>
+                </li>
+            </ul>
+        </header>
     );
 }
 
