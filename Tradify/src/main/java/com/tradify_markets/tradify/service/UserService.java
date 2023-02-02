95109dffd87ddffb66d8415a19b1f0160cc6058e
Santiago Lopez Paredes <46728986+SantiagoLopez-hub@users.noreply.github.com>
2023-02-02 18:16:00 +0000

    Add findByUsername method

--- Tradify/src/main/java/com/tradify_markets/tradify/service/UserService.java
+++ Tradify/src/main/java/com/tradify_markets/tradify/service/UserService.java
@@ -26,6 +26,10 @@ public class UserService implements UserDetailsService {
         return userRepository.save(user);
     }
 
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
