8b9f60a06539c43a2162d2d8a2db929f27fbb1ff
Santiago <silp4477@gmail.com>
2023-02-04 11:33:39 +0000

    Create UserServiceTest.java

---
+++ Tradify/src/test/java/com/tradify_markets/tradify/service/UserServiceTest.java
@@ -0,0 +1,49 @@
+package com.tradify_markets.tradify.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void findAll() {
+        // given
+        assert true;
+
+        // when
+        // then
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void userShares() {
+    }
+
+    @Test
+    void userOrders() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+}
