e21e718be993e3de59ee8fe4dd03a118651ccfc7
Santiago <silp4477@gmail.com>
2023-02-05 18:14:09 +0000

    Prevent loop

--- Tradify/src/main/java/com/tradify_markets/tradify/service/OrderService.java
+++ Tradify/src/main/java/com/tradify_markets/tradify/service/OrderService.java
@@ -2,17 +2,21 @@ package com.tradify_markets.tradify.service;
 
 import com.tradify_markets.tradify.model.Order;
 import com.tradify_markets.tradify.repository.OrderRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
-@RequiredArgsConstructor
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
     private final UserService userService;
+
+    public OrderService(OrderRepository orderRepository, @Lazy UserService userService) {
+        this.orderRepository = orderRepository;
+        this.userService = userService;
+    }
 
     public Order findByUser(Integer id) {
         return orderRepository.findByUser(userService.findById(id));
--- Tradify/src/main/java/com/tradify_markets/tradify/service/UserShareService.java
+++ Tradify/src/main/java/com/tradify_markets/tradify/service/UserShareService.java
@@ -2,17 +2,21 @@ package com.tradify_markets.tradify.service;
 
 import com.tradify_markets.tradify.model.UserShare;
 import com.tradify_markets.tradify.repository.UserShareRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
-@RequiredArgsConstructor
 @Transactional
 public class UserShareService {
     private final UserShareRepository userShareRepository;
     private final UserService userService;
+
+    public UserShareService(UserShareRepository userShareRepository, @Lazy UserService userService) {
+        this.userShareRepository = userShareRepository;
+        this.userService = userService;
+    }
 
     public UserShare findByUser(Integer id) {
         return userShareRepository.findByUser(userService.findById(id));
