253766df9911eef6d6dfbce5e3338bc1fa98b848
Santiago Lopez Paredes <46728986+SantiagoLopez-hub@users.noreply.github.com>
2023-02-20 01:12:30 +0000

    Set up login functionality

--- Tradify/frontend/src/Pages/Login.jsx
+++ Tradify/frontend/src/Pages/Login.jsx
@@ -1,30 +1,44 @@
-import ApiCall from "../Components/ApiCall";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
-    const [login, isLoading, error] = ApiCall(
-        "POST",
-        "/login",
-        new URLSearchParams({
-            username: "username",
-            password: "1234",
-        })
-    );
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    if (error) {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    {error}, Please try again later.
-                </header>
-            </div>
-        );
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError(false);
 
-    localStorage.setItem("access_token", login?.access_token);
+        axios
+            .post(
+                process.env.REACT_APP_DOMAIN + "/login",
+                new URLSearchParams({
+                    username: username,
+                    password: password,
+                })
+            )
+            .then((response) => {
+                localStorage.setItem(
+                    "access_token",
+                    response?.data?.access_token
+                );
+                localStorage.setItem(
+                    "refresh_token",
+                    response?.data?.refresh_token
+                );
+                setIsLoggedIn(true);
+            })
+            .catch((err) => {
+                setError(err.message);
+            });
+    };
 
-    return (
+    return !isLoggedIn ? (
         <header className="App-header">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="username" className="form-label">
                         Username
@@ -34,6 +48,9 @@ const Login = () => {
                         className="form-control"
                         id="username"
                         aria-describedby="username"
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                        }}
                     />
                 </div>
                 <div className="mb-3">
@@ -44,13 +61,24 @@ const Login = () => {
                         type="password"
                         className="form-control"
                         id="password"
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
                     />
                 </div>
                 <button type="submit" className="btn btn-primary">
                     Submit
                 </button>
+
+                {error && (
+                    <div className="alert alert-danger" role="alert">
+                        <p>{error}</p>
+                    </div>
+                )}
             </form>
         </header>
+    ) : (
+        <Navigate to="/" />
     );
 };
 
