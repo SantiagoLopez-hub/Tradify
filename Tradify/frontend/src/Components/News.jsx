74452a8c3ce8e4c8d1a211232b1762d8b949c2f4
Santiago Lopez Paredes <46728986+SantiagoLopez-hub@users.noreply.github.com>
2023-03-10 02:57:43 +0000

    Show all news in table

--- Tradify/frontend/src/Components/News.jsx
+++ Tradify/frontend/src/Components/News.jsx
@@ -1,30 +1,37 @@
 import ApiCall from "./ApiCall";
 
 const News = ({ share_id }) => {
-    const [news, isLoading, error] = ApiCall("GET", "/news/share/" + share_id, {
+    const [news] = ApiCall("GET", "/news/share/" + share_id, {
         headers: {
             Authorization: `Bearer ${localStorage.getItem("access_token")}`,
         },
     });
 
-    return (
-        <div>
-            <h1>News</h1>
-            {error && <p>{error}</p>}
-            {isLoading ? (
-                <p>Loading...</p>
-            ) : (
-                <ul>
-                    {news.map((news) => (
-                        <li key={news.id}>
-                            <p>{news.title}</p>
-                            <p>{news.content}</p>
-                            <p>{news.date}</p>
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
+    return news.length === 0 ? (
+        "No news published yet"
+    ) : (
+        <table className="table container">
+            <thead>
+                <tr>
+                    <th>News</th>
+                    <th colSpan={2}></th>
+                </tr>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Content</th>
+                    <th scope="col">Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                {news.map((news) => (
+                    <tr className="" key={news.id}>
+                        <td>{news.title}</td>
+                        <td>{news.content}</td>
+                        <td>{news.date}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 };
 
