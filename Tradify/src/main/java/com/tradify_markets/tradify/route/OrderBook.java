befc7f30d12df03b8182847cfe2510cdb8fcb21d
Santiago <silp4477@gmail.com>
2023-03-12 16:23:27 +0000

    Set order type and status

--- Tradify/src/main/java/com/tradify_markets/tradify/route/OrderBook.java
+++ Tradify/src/main/java/com/tradify_markets/tradify/route/OrderBook.java
@@ -1,7 +1,8 @@
 package com.tradify_markets.tradify.route;
 
-import com.tradify_markets.tradify.model.Order;
-import com.tradify_markets.tradify.model.User;
+import com.tradify_markets.tradify.model.*;
+import com.tradify_markets.tradify.repository.OrderStatusRepository;
+import com.tradify_markets.tradify.repository.OrderTypeRepository;
 import com.tradify_markets.tradify.service.OrderService;
 import com.tradify_markets.tradify.service.UserService;
 import lombok.AllArgsConstructor;
@@ -10,21 +11,36 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import java.util.Base64;
-
 @Controller
 @AllArgsConstructor
 public class OrderBook {
     private OrderService orderService;
     private UserService userService;
+    private OrderTypeRepository orderTypeRepository;
+    private OrderStatusRepository orderStatusRepository;
 
     @MessageMapping("/create")
     @SendTo("/subscribe-orders/order-book")
     public Order create(@Payload Order order) {
+        // Get User
         String username = order.getUser().getUsername();
         User user = userService.findByUsername(username);
         assert user != null;
+
+        // Get Order Type
+        Integer type = order.getOrderType().getId();
+        OrderType orderType = orderTypeRepository.findById(type).orElse(null);
+        assert orderType != null;
+
+        // Get Status
+        Integer status = order.getOrderType().getId();
+        OrderStatus orderStatus = orderStatusRepository.findById(status).orElse(null);
+        assert orderStatus != null;
+
+        // Set all values
         order.setUser(user);
+        order.setOrderType(orderType);
+        order.setStatus(orderStatus);
 
         return orderService.create(order);
     }
