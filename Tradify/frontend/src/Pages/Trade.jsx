ef6403e019095575cdddca8f01277a12faa75ce5
Santiago <silp4477@gmail.com>
2023-03-12 18:04:23 +0000

    Create orders

--- Tradify/frontend/src/Pages/Trade.jsx
+++ Tradify/frontend/src/Pages/Trade.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import { over } from "stompjs";
+import SockJS from "sockjs-client";
 import ApiCall from "../Components/ApiCall";
 import ApiError from "../Components/ApiError";
 import News from "../Components/News";
-import OrderBook from "../Components/OrderBook";
 import OrderForm from "../Components/OrderForm";
 import Orders from "../Components/Orders";
 import OwnwedShares from "../Components/OwnwedShares";
@@ -16,9 +18,63 @@ const Trade = () => {
         },
     });
 
+    const [err, setErr] = useState(false);
+    let stompClient = null;
+
+    const connect = () => {
+        const socket = new SockJS(
+            process.env.REACT_APP_DOMAIN + "/websocket-connection"
+        );
+
+        stompClient = over(socket);
+
+        stompClient.connect(
+            {
+                Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+            },
+            onConnected,
+            onError
+        );
+    };
+
+    const onError = (err) => setErr(err);
+
+    const onConnected = () => {
+        stompClient.subscribe("/subscribe-orders/order-book", onOrderReceived);
+    };
+
+    const onOrderReceived = (order) => {
+        console.log("Message recieved:", JSON.parse(order.body));
+    };
+
+    const createOrder = (order_type, order) => {
+        order.share = share;
+        order.status = {
+            id: 1,
+        };
+        order.orderType = {
+            id: order_type,
+        };
+
+        stompClient.send(
+            "/stocks/order-book/create",
+            {},
+            JSON.stringify(order)
+        );
+    };
+
+    const disconnect = () => {
+        if (stompClient !== null) stompClient.disconnect();
+
+        console.log("Disconnected");
+    };
+
+    connect();
+
     return (
         <div>
             {error && <ApiError error={error} />}
+            {err && <ApiError error={err} />}
 
             {isLoading ? (
                 <p>Loading...</p>
@@ -28,10 +84,9 @@ const Trade = () => {
                         {share.name} | {share.symbol} | {share.exchange}
                     </p>
 
-                    <OrderBook share={share} />
                     <PriceGraph share_id={share.id} />
                     <OwnwedShares share_id={share.id} />
-                    <OrderForm />
+                    <OrderForm createOrder={createOrder} />
                     <Orders share_id={share.id} />
                     <News share_id={share.id} />
                 </>
