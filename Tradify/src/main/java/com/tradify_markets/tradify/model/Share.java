ce83add450cd8ff96b0c1ac50166f11c8716df24
Santiago Lopez Paredes <46728986+SantiagoLopez-hub@users.noreply.github.com>
2023-03-17 17:02:10 +0000

    Update Share.java

--- Tradify/src/main/java/com/tradify_markets/tradify/model/Share.java
+++ Tradify/src/main/java/com/tradify_markets/tradify/model/Share.java
@@ -19,7 +19,7 @@ public class Share {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @OneToMany(targetEntity = NewsModel.class, cascade = CascadeType.ALL)
+    @OneToMany(targetEntity = NewsModel.class, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @Builder.Default
     private List<NewsModel> news = new ArrayList<>();
 
